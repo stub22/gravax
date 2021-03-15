b450cc237d32ad426e9e1893afecd88bf99f7a95
stub22 <1082227+stub22@users.noreply.github.com>
2021-03-14 23:06:55 -0700

    Added license info to scala source files

--- grvx_mvn/grvx_grind/src/main/scala/fun/gravax/gravapp/wsrv/GravdatWebTupleStuff.scala
+++ grvx_mvn/grvx_grind/src/main/scala/fun/gravax/gravapp/wsrv/GravdatWebTupleStuff.scala
@@ -1,3 +1,4 @@
+// Copyright 2021 Stu Btwotwo.  Distributed under Apache License 2.0.  See bottom of this file.
 package fun.gravax.gravapp.wsrv
 
 import akka.http.scaladsl.{server => dslServer}
@@ -37,3 +38,20 @@ trait GravTupleRouteBuilder extends RouteWeaver {
 	protected lazy val myGravTplRt: dslServer.Route = myGravTplRtMkr.makeWbTplRt(ourSlf4JLog)
 	def getGravTplRt : dslServer.Route = myGravTplRt
 }
+/***
+--------------------------------------------------------------
+Copyright 2021 Stu Btwotwo
+See GRAVAX_LICENSE_APACHE2.txt and GRAVAX_CREDITS.txt
+--------------------------------------------------------------
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***/
