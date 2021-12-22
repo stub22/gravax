ffec04f3785095bf531f9180fac298f4acc78cbd
stub22 <1082227+stub22@users.noreply.github.com>
2021-12-22 14:54:33 -0800

    Sketching a minimal pure symbolic number typeset

---
+++ grvx_mvn/grvx_aws/src/main/scala/fun/gravax/gravnum/PureNumSymAlignStuff.scala
@@ -0,0 +1,21 @@
+package fun.gravax.gravnum
+
+private trait PureNumSymAlignStuff
+
+/*
+Define alignment from PureNum types to some well known URIs.
+
+AxLam is a dependent-typed Lambda calculus
+Universes like KDDTT
+Functions are defined in RDF graphs
+properties may act like functions
+functions take one arg but are eval in a Ctx
+a function invocation may be recorded as a triple
+     :input  :functionPred  :result
+
+     :pipn_8  :cubeRoot :pipn_2
+     :pipn_9  :sqRoot   :branch_pair(:pipn_3  :nipn_neg3)
+ */
+trait Whoa {
+
+}
---
+++ grvx_mvn/grvx_aws/src/main/scala/fun/gravax/gtst/TstPureSymsYow.scala
@@ -0,0 +1,12 @@
+package fun.gravax.gtst
+
+private trait TstPureSymsYow // SourceFileNameMarkerTrait
+
+object RunSymTstsNow {
+	def main(args: Array[String]): Unit = {
+		goDoStuff
+	}
+	def goDoStuff : Unit = {
+		println ("RunSymTstsNow doing stuff yay")
+	}
+}
