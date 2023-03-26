12481273e492ec40e8c02674258c6b5758c4ba8d
stub22 <1082227+stub22@users.noreply.github.com>
2023-03-25 17:06:59 -0700

    Warming up ZSink with simple average of gaussian-distributed BigDecimals

--- grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zpol/dsktch/FirstWhackAtDSktch.scala
+++ grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zpol/dsktch/FirstWhackAtDSktch.scala
@@ -1,6 +1,8 @@
 package fun.gravax.zpol.dsktch
 
-import org.apache.datasketches.quantiles.ItemsSketch
+import org.apache.datasketches.quantiles.{ItemsSketch, ItemsUnion}
+
+import java.util.Comparator
 
 private trait FirstWhackAtDSktch
 
@@ -13,42 +15,63 @@ How about wrapping as a ZStream sink?
 
  */
 
-trait ItemSketchReader[T] {
-
+trait QuantileSketchReader[T] {
 	type InArrT = Array[T with Object]
 	type OutArrT = Array[_ <: T]
+
+	def getK: Int
+	def getN : Long
+	def isEmpty: Boolean
+	def getRetainedItems: Int
+	def getMaxValue: T
+	def getMinValue: T
+	def getRank(t : T) : Double
+	def getQuantile(fraction : Double): T
+	def getQuantileLowerBound(fraction : Double): T
+	def getQuantileUpperBound(fraction : Double): T
+	def getNormalizedRankError(pmf : Boolean) : Double
+
+	def getPMF(splitPoints: InArrT): Array[Double]
+	def getCDF(splitPoints: InArrT): Array[Double]
+	def getQuantiles(fRanks : Array[Double]): OutArrT
+	def getQuantiles(evenlySpaced : Int): OutArrT
+
+}
+trait QuantileSketchReaderHasMut[T] extends QuantileSketchReader[T] {
+
+
 	// Offers only the output features of the sketch.
 	protected def getMutSketch : ItemsSketch[T]
 
 
 
 	// Returns the configured value of K
-	def getK: Int = getMutSketch.getK
+	override def getK: Int = getMutSketch.getK
 	// Returns the length of the input stream so far.
-	def getN : Long = getMutSketch.getN
-	def isEmpty: Boolean = getMutSketch.isEmpty
+	override def getN : Long = getMutSketch.getN
+	override def isEmpty: Boolean = getMutSketch.isEmpty
 	// Computes the number of retained entries (samples) in the sketch
-	def getRetainedItems: Int = getMutSketch.getRetainedItems
+	override def getRetainedItems: Int = getMutSketch.getRetainedItems
 
-	def getMaxValue: T = getMutSketch.getMaxValue
-	def getMinValue: T = getMutSketch.getMinValue
+	override def getMaxValue: T = getMutSketch.getMaxValue
+	override def getMinValue: T = getMutSketch.getMinValue
 
 	// Returns an approximation to the normalized (fractional) rank of the given value from 0 to 1 inclusive.
-	def getRank(t : T) : Double = getMutSketch.getRank(t)
+	override def getRank(t : T) : Double = getMutSketch.getRank(t)
 
 	// This returns an approximation to the value of the data item that would be preceded by the given fraction of a hypothetical sorted version of the input stream so far.
-	def getQuantile(fraction : Double): T = getMutSketch.getQuantile(fraction)
+	override def getQuantile(fraction : Double): T = getMutSketch.getQuantile(fraction)
 
 	// Gets the lower bound of the value interval in which the true quantile of the given rank exists with a confidence of at least 99%.
-	def getQuantileLowerBound(fraction : Double): T = getMutSketch.getQuantileLowerBound(fraction)
+	override def getQuantileLowerBound(fraction : Double): T = getMutSketch.getQuantileLowerBound(fraction)
 
 	// Gets the upper bound of the value interval in which the true quantile of the given rank exists with a confidence of at least 99%.
-	def getQuantileUpperBound(fraction : Double): T = getMutSketch.getQuantileUpperBound(fraction)
+	override def getQuantileUpperBound(fraction : Double): T = getMutSketch.getQuantileUpperBound(fraction)
 
 	// Gets the approximate rank error of this sketch normalized as a fraction between zero and one.
 	// pmf - if true, returns the "double-sided" normalized rank error for the getPMF() function.
 	// Otherwise, it is the "single-sided" normalized rank error for all the other queries.
-	def getNormalizedRankError(pmf : Boolean) : Double = getMutSketch.getNormalizedRankError(pmf)
+	override def getNormalizedRankError(pmf : Boolean) : Double = getMutSketch.getNormalizedRankError(pmf)
 
 	// With all these methods that return arrays, where is guarantee that these returned arrays are immutable?
 
@@ -63,7 +86,7 @@ Returns:
 an array of m+1 doubles each of which is an approximation to the fraction of the input stream values (the mass) that fall into one of those intervals. The definition of an "interval" is inclusive of the left splitPoint and exclusive of the right splitPoint, with the exception that the last interval will include maximum value.
  */
 	// public double[] getPMF(T[] splitPoints) {
-	def getPMF(splitPoints: InArrT): Array[Double] = getMutSketch.getPMF(splitPoints)
+	override def getPMF(splitPoints: InArrT): Array[Double] = getMutSketch.getPMF(splitPoints)
 
 
 	/* getCDF     https://datasketches.apache.org/api/java/snapshot/apidocs/org/apache/datasketches/quantiles/ItemsSketch.html
@@ -74,7 +97,7 @@ If the sketch is empty this returns null.
 The value at array position j of the returned CDF array is the sum of the returned values in positions 0 through j of the returned PMF array.
 	 */
 	//  found   : Array[T] 	// required: Array[T with Object] //Note: T >: T with Object, but class Array is invariant in type T.
-	def getCDF(splitPoints: InArrT): Array[Double] = getMutSketch.getCDF(splitPoints)
+	override def getCDF(splitPoints: InArrT): Array[Double] = getMutSketch.getCDF(splitPoints)
 
 /*
 	This is a more efficient multiple-query version of getQuantile().
@@ -90,20 +113,79 @@ The value at array position j of the returned CDF array is the sum of the return
 
  */
 	//  found   : Array[T with Object]   ...   but class Array is invariant in type T.
-	def getQuantiles(fRanks : Array[Double]): OutArrT = getMutSketch.getQuantiles(fRanks)
+	override def getQuantiles(fRanks : Array[Double]): OutArrT = getMutSketch.getQuantiles(fRanks)
 /*
 evenlySpaced - an integer that specifies the number of evenly spaced fractional ranks.
 This must be a positive integer greater than 1. A value of 2 will return the min and the max value.
 A value of 3 will return the min, the median and the max value, etc.
  */
-	def getQuantiles(evenlySpaced : Int): OutArrT = getMutSketch.getQuantiles(evenlySpaced)
-
+	override def getQuantiles(evenlySpaced : Int): OutArrT = getMutSketch.getQuantiles(evenlySpaced)
 
 }
-trait ItemSketchWriter[T] {
-	def addItem(itm : T)
+
+trait QuantileSketchWriter[T] {
+	// This writer contains the (approximate) result of items written so far.
+	// The writer does not allow mutation.  If you call ".addItem" twice (on THIS writer), you will get two different
+	// QSWs out. This usually implies copying costs.  Note that we could make a lossless QSW that always keeps all items.
+	def addItem(itm : T) : QuantileSketchWriter[T]
+	// Merge produces a new writer.
+	def merge(otherQSW : QuantileSketchWriter[T]) : QuantileSketchWriter[T]
+	// Return an immutable sketch reader
+	def getSketchReader : QuantileSketchReader[T]
 }
 
-trait QuantileResultGetter {
+case class EmptyQSW[T](numBins : Int, compr : Comparator[_ >: T]) extends QuantileSketchWriter[T] {
+	override def addItem(itm: T): QuantileSketchWriter[T] = {
+		val sktchOfOne: ItemsSketch[T] = ItemsSketch.getInstance[T](numBins, compr)
+		sktchOfOne.update(itm)
+		new QntlSktchWrtrImplHeavy[T](sktchOfOne)
+	}
 
+	override def merge(otherQSW: QuantileSketchWriter[T]): QuantileSketchWriter[T] =  ???
+
+	override def getSketchReader: QuantileSketchReader[T] = ???
+}
+
+class QntlSktchWrtrImplHeavy[T](val mutSktch : ItemsSketch[T]) extends QuantileSketchWriter[T] {
+	override def addItem(itm: T): QuantileSketchWriter[T] = {
+		val helpyUnion = ItemsUnion.getInstance(mutSktch)
+		helpyUnion.update(itm)
+		val outSktch: ItemsSketch[T] = helpyUnion.getResult
+		val outWrtr = new QntlSktchWrtrImplHeavy(outSktch)
+		outWrtr
+	}
+
+	override def merge(otherQSW: QuantileSketchWriter[T]): QuantileSketchWriter[T] = {
+		val union = ItemsUnion.getInstance(mutSktch)
+		otherQSW match {
+			case hvy : QntlSktchWrtrImplHeavy[T] => union.update(hvy.mutSktch)
+			case _ => throw new Exception(s"Cannot merge with otherQSW of class ${otherQSW.getClass}")
+		}
+		val outSktch: ItemsSketch[T] = union.getResult
+		val outWrtr = new QntlSktchWrtrImplHeavy(outSktch)
+		outWrtr
+	}
+
+	override def getSketchReader: QuantileSketchReader[T] = {
+		val msktch = mutSktch
+		new QuantileSketchReaderHasMut[T] {
+			override protected def getMutSketch: ItemsSketch[T] = msktch
+		}
+	}
+}
+trait HeavySktchMkr {
+	def mkEmptyQSW[T](numBins : Int, compr : Comparator[_ >: T]) : QuantileSketchWriter[T] = EmptyQSW(numBins, compr)
+}
+
+trait MakeSomeQuantSktchs {
+	def go : Unit = {
+		val hsm = new HeavySktchMkr {}
+		val binCnt = 8
+		// TODO: Consider different syntax forms for defining Comparator in Scala
+		val bdComp = new Comparator[BigDecimal] {
+			override def compare(o1: BigDecimal, o2: BigDecimal): Int = o1.compare(o2)
+		}
+		def qswBD = hsm.mkEmptyQSW(8, bdComp)
+
+	}
 }
---
+++ grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zpol/dsktch/SketchZStrmStf.scala
@@ -0,0 +1,89 @@
+package fun.gravax.zpol.dsktch
+
+import zio.stream.{ZSink, ZStream}
+import zio.{UIO, ZIO, ZIOAppDefault}
+import zio.{Random => ZRandom}
+
+import java.io.IOException
+import java.math.{MathContext, RoundingMode}
+
+
+private trait SketchZStrmStf
+
+object RunDSktchZstrmDemo extends ZIOAppDefault {
+
+	def run = myAppLogic
+
+	val myAppLogic: ZIO[Any, IOException, Unit] = {
+		for {
+			_	<- ZIO.log("RunDSktchZstrmDemo BEGIN")
+			zs  <- sumPositiveInts(1000)
+			_	<- ZIO.log(s"Sum result: ${zs}")
+			dn <- dumNum
+			_	<- ZIO.log(s"Dumb rando: ${dn}")
+			adn <- avgDumNum
+			_	<- ZIO.log(s"Average dumNum = ${adn}")
+		} yield ()
+	}
+
+	def sumPositiveInts(posInt : Int) : UIO[Int] = {
+		val rangeToSum = 1 to posInt
+		val stream: ZStream[Any, Nothing, Int] = ZStream.fromIterable(rangeToSum)
+		val sink: ZSink[Any, Nothing, Int, Nothing, Int] = ZSink.sum[Int]
+		val sum: ZIO[Any, Nothing, Int] = stream.run(sink)
+		val sumUIO : UIO[Int] = sum
+		sumUIO
+	}
+
+	def gaussianBD(zrnd : ZRandom, mathCtx : MathContext,  mean : BigDecimal, stdDev : BigDecimal): UIO[BigDecimal] = {
+		val stdNumEff: UIO[Double] = zrnd.nextGaussian
+		stdNumEff.map(num => {
+			// https://blogs.oracle.com/javamagazine/post/four-common-pitfalls-of-the-bigdecimal-class-and-how-to-avoid-them
+			val stdBD = BigDecimal(num, mathCtx)
+			val scaledAndShiftedBD = stdBD.*(stdDev).+(mean)
+			scaledAndShiftedBD
+		})
+	}
+	def dumNum : UIO[BigDecimal] = {
+		val mean = BigDecimal("-5.0")
+		val dev = BigDecimal("4.5")
+		val mathCtx = new MathContext(8, RoundingMode.HALF_UP)
+		gaussianBD(ZRandom.RandomLive, mathCtx, mean, dev)
+	}
+	def avgDumNum : UIO[BigDecimal] = {
+		val strm: ZStream[Any, Nothing, BigDecimal] = ZStream.repeatZIO(dumNum)
+		val shortStrm = strm.take(500)
+		val zeroPair : (Int, BigDecimal) = (0, BigDecimal("0.0"))
+		// foldLeft[In, S](z: => S)(f: (S, In) => S)
+		val accumSink = ZSink.foldLeft[BigDecimal, (Int, BigDecimal)](zeroPair)((prevStPair, nxtBD) => {
+			(prevStPair._1 + 1, prevStPair._2.+(nxtBD))
+		})
+		val avgSink = accumSink.map(pair => {pair._2./(pair._1)})
+		val shortAvg = shortStrm.run(avgSink)
+		shortAvg
+	}
+
+}
+/*
+https://zio.dev/reference/stream/
+
+Streams and Sinks are duals in category theory. One produces values, and the other one consumes them.
+They are mere images of each other. They both have to exist. A streaming library cannot be complete unless it has
+streams and sinks. That is why ZIO has a sort of better design than FS2 because FS2 has a stream, but it doesn't
+have a sink. Its Sink is just faked. It doesn't actually have a real sink.
+ZIO has a real sink, and we can compose them to generate new sinks.
+
+https://zio.dev/reference/core/zio/task/
+type Task[+A] = ZIO[Any, Throwable, A]
+So a Task is equal to a ZIO that doesn't need any requirement, and may fail with a Throwable, or succeed with an A value.
+
+https://zio.dev/reference/core/zio/uio/
+type UIO[+A] = ZIO[Any, Nothing, A]
+
+So UIO is equal to a ZIO that doesn't need any requirement (because it accepts Any environment) and that cannot fail
+(because in Scala the Nothing type is uninhabitable, i.e. there can be no actual value of type Nothing).
+It succeeds with A.
+
+ZIO values of type UIO[A] are considered infallible. Values of this type may produce an A, but will never fail.
+
+ */
