5190fb7fb1b2182311c1ea879929478be5e76896
stub22 <1082227+stub22@users.noreply.github.com>
2023-04-27 16:37:09 -0700

    Draft of BinNode and variance estimate

--- grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zdynamo/BinDataStuff.scala
+++ grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zdynamo/BinDataStuff.scala
@@ -1,7 +1,9 @@
 package fun.gravax.zdynamo
 
+import zio.{UIO, Random => ZRandom}
 import zio.dynamodb.Item
 
+import java.math.{MathContext, RoundingMode}
 import scala.collection.immutable.{Map => SMap}
 
 private trait BinDataStuff
@@ -11,8 +13,10 @@ trait BinData {
 	def getObsTime : String
 	def getPredTime : String
 	def getCalcTime : String
-	def getBinSeq : String
-	def getParentBinSeq : String
+	def getBinSeqTxt : String
+	def getBinSeqInt : Int
+	def getParentBinSeqTxt : String
+	def getParentBinSeqInt : Int
 	def getBinFlavor : String // TODO: Make enum-ish
 	def getRelWt : BigDecimal
 	def getAbsWt : BigDecimal
@@ -28,16 +32,117 @@ case class EzBinData(scenID : String, timeDat : BinTimeData, seqDat : BinSeqInfo
 	override def getObsTime: String = timeDat.obsTime
 	override def getPredTime: String = timeDat.predTime
 	override def getCalcTime: String = timeDat.calcTime
-	override def getBinSeq: String = seqDat.binSeq
-	override def getParentBinSeq: String = seqDat.parentBinSeq
+	override def getBinSeqTxt: String = seqDat.binSeq
+	override def getBinSeqInt: Int = ???
+	override def getParentBinSeqTxt: String = seqDat.parentBinSeq
+	override def getParentBinSeqInt: Int = ???
 	override def getMass: BigDecimal = massDat.binMass
 	override def getRelWt: BigDecimal = massDat.relWt
 	override def getAbsWt: BigDecimal = massDat.absWt
 	override def getBinFlavor: String = meat.binFlavor
 	override def getAnnRetMeans: SMap[String, BigDecimal] = meat.meatMap
+
+
+}
+trait NameScopeHmm {
+	type KeySym = String
+	type RowEntry = (KeySym, BigDecimal)
+	type Row = Seq[RowEntry]   // For some set of keySyms, in some useful order that is not specified by type.
+
+	type DBinID = Int
+	type DBinWt = BigDecimal
+	type DBinDat = (DBinID, DBinWt, Row)
+}
+trait VecDistribFragment extends NameScopeHmm {
+
+
+	// We expect Assets (meatKeys) to be identical across all bins
+	def	getFullKeySymSet : Set[KeySym] // The syms do not have a canonical ordering.  Client may use alphabetic, or...
+	def projectMean(keySyms : Seq[KeySym]) : Row // Often this is available directly from VecDistrib-bin-0
+
+}
+class BinNode(myDat : BinData, parent_opt : Option[BinNode], kids : Iterable[BinNode], meatKeyOrder : Ordering[String])  extends VecDistribFragment  {
+
+	// Assume meatKeys are same across all bins
+	override def getFullKeySymSet : Set[KeySym] = myDat.getAnnRetMeans.keySet
+
+
+	override def projectMean(keySyms: Seq[KeySym]): Row = {
+		// Will throw on failed lookup
+		val meatMap = myDat.getAnnRetMeans
+		val entrySeq : Row = keySyms.map(ksym => {
+			val numValAtSym : BigDecimal = meatMap.get(ksym).get // This second .get will throw if no value present
+			(ksym, numValAtSym)
+		})
+		entrySeq
+	}
+
+	lazy val myFullBinDat : DBinDat = {
+		val allSymsUnsorted = getFullKeySymSet
+		val orderedSyms = allSymsUnsorted.toSeq.sorted(meatKeyOrder)
+		projectToDBD(orderedSyms)
+	}
+
+	def projectToDBD (orderedSyms : Seq[KeySym]) : DBinDat = {
+		val meanDat = projectMean(orderedSyms)
+		(myDat.getBinSeqInt, myDat.getRelWt, meanDat)
+	}
+}
+
+trait VecDistrib extends VecDistribFragment {
+
+	// TODO:  Refine to a triangular rep.
+	type CovarRow = (KeySym, Row)
+	type CovarMatrix = Seq[CovarRow]
+
+	def projectCovarMatrix(keySyms : Seq[KeySym], maxLevels : Int) : CovarMatrix // Use at most maxLevels of the bin structure
+	def projectRootBin(keySyms : Seq[KeySym]) : DBinDat // Often this is same as mean.
+	def projectChildBins(parentBinID : DBinID, keySyms : Seq[KeySym]) : Seq[DBinDat]
 }
 
 
+// Assume meatKeys are same across all bins
+class VecDistribImpl(rootBN : BinNode) extends VecDistrib {
+	override def getFullKeySymSet: Set[KeySym] = rootBN.getFullKeySymSet
+
+	override def projectMean(keySyms: Seq[KeySym]): Row = rootBN.projectMean(keySyms)
+
+	override def projectCovarMatrix(keySyms: Seq[KeySym], maxLevels: DBinID): CovarMatrix = ???
+
+	override def projectRootBin(keySyms: Seq[KeySym]): DBinDat = rootBN.projectToDBD(keySyms)
+
+	override def projectChildBins(parentBinID: DBinID, keySyms: Seq[KeySym]): Seq[DBinDat] = ???
+}
+
+trait ToBinData {
+	def pullTimeData(itm : Item) : BinTimeData = {
+		// If field is missing, we throw?  Or should we return option/either?
+		???
+	}
+
+}
+trait GenBinData {
+
+	def mkRandMeatMap(meatKeys : Seq[String], min : BigDecimal, max : BigDecimal) // Use normal distrib?
+
+	def dumNum : UIO[BigDecimal] = {
+		val mean = BigDecimal("-5.0")
+		val dev = BigDecimal("4.5")
+		val mathCtx = new MathContext(8, RoundingMode.HALF_UP)
+		gaussianBD(ZRandom.RandomLive, mathCtx)(mean, dev)
+	}
+	def gaussianBD(zrnd : ZRandom, mathCtx : MathContext)(mean : BigDecimal, stdDev : BigDecimal): UIO[BigDecimal] = {
+		val stdNumEff: UIO[Double] = zrnd.nextGaussian
+		stdNumEff.map(num => {
+			// https://blogs.oracle.com/javamagazine/post/four-common-pitfalls-of-the-bigdecimal-class-and-how-to-avoid-them
+			val stdBD = BigDecimal(num, mathCtx)
+			val scaledAndShiftedBD = stdBD.*(stdDev).+(mean)
+			scaledAndShiftedBD
+		})
+	}
+}
+
+/*
 import java.time.{Instant => JInstant}
 
 case class PredictionTimeInfo(obsTime : JInstant, calcTime : JInstant, endTime : JInstant)
@@ -63,3 +168,4 @@ class MultiAssetBinModel extends BinModel {
 
 	override def makeZDI(br: BinRecord): Item = ???
 }
+*/
---
+++ grvx_mvn/grvx_zpol/src/main/scala/fun/gravax/zdynamo/DistroCalcStuff.scala
@@ -0,0 +1,79 @@
+package fun.gravax.zdynamo
+
+private trait DistroCalcStuff
+
+
+trait HoldingOptimizer extends NameScopeHmm {
+	type	AssetID = KeySym
+	type 	MultiAssetAnnRetDist = VecDistrib
+	type	AnnRet = BigDecimal
+	type	AssetRet = (AssetID, AnnRet) // Same as RowEntry
+	type	HoldWt = (AssetID, BigDecimal)
+	type 	AnnRetVar = BigDecimal
+	type	AnnRetDist
+
+	type	SharpeRatio = BigDecimal
+
+	type OptimResult =  (Map[AssetID, BigDecimal], AnnRetDist, SharpeRatio)
+
+	// Could use gradient descent or another method.
+	// Client could pass in a Seq[AssetID] in order to control ordering in the output.
+	def optimizeSharpe(availAssets : Set[AssetID], maRetDist : MultiAssetAnnRetDist) : OptimResult = {
+		val availAssetSeq = availAssets.toSeq
+		// Expected portfolio return will be dot-product of our portfolio asset weights with these mean asset returns.
+		// This is a purely linear calculation, with a single number as result.
+		val rootDBD = maRetDist.projectRootBin(availAssetSeq)
+		val (rootBinID, rootBinWt, rootBinRow) = rootDBD
+		val bd1 = BigDecimal("1.0")
+		assert (rootBinWt == bd1)
+		// Computing the portfolio variance is more complicated.  Each child bin represents some PART of the total
+		// distribution (but the support of the bins may be overlapping).  For a given set of portfolio asset weights,
+		// the variance will be the expected squared difference over all possible outcomes.  This can be treated as
+		// the weighted sum of the variance found in each bin (computed relative to the GLOBAL mean of the distribution).
+		// Using only the mean return of the bin as a representative gives us an imperfect estimate of the
+		// bin-variance-relative-to-global, in which we skip over the squared-value integral.
+		// If the bins are chosen to be heavily overlapping, then our estimated variance will tend to undershoot.
+		// Our method is more accurate when the bins are mostly disjoint.  We can try to improve by asking for
+		// additional information with the bins.
+		// Note that Sortino ratio emphasizes the downside variance, whereas Sharpe treats all variance equally.
+		val childDBDs : Seq[maRetDist.DBinDat] = maRetDist.projectChildBins(rootBinID, availAssetSeq)
+		// Now we need either
+		// 1) An iterative algorithm, e.g. gradient descent.
+		// 2) A gaussian approximation using covariances (which turns optimization into a matrix-inverse problem).
+		// 3) An optimization expression that we can differentiate and solve for 0 to find candidate extrema.
+		// 4) A monte-carlo guesser.  This might be the easiest way to start.
+		// In some scenarios we may allow negative weights.
+		val distsFromMean = ??? // childDBDs.map()
+		???
+	}
+
+	// TODO:  Make this expression differentiable in the asset weights, so that we can use gradient descent.
+	def estimVariance(meanRet : AnnRet, assetWeights : Seq[HoldWt], binDats : Seq[DBinDat]) : AnnRetVar = {
+		val weightedSquaredDevs : Seq[AnnRetVar] = binDats.map(binDat => {
+			val binRow: Row = binDat._3
+			val binARs : Seq[AssetRet] = binRow
+			val estimRetForBin = calcReturnAtVec(assetWeights, binARs)
+			val estimDiff = estimRetForBin - meanRet
+			val estimDiffSq = estimDiff.*(estimDiff) // This is where the estimation goes a little wobbly.
+			// ...because integral of squared diff (over the whole bin) is not the same as square of mean diff of the bin.
+			// For example, if the bin contains very-large-diff outliers they will be under-represented in this variance.
+			// Maybe if variance is >= 1 this estim turns out to be a lower bound, but if var < 1 it's an upper bound?
+			// However if the client proceeds to use additional layers of the BinDat it can get a more precise estimate.
+			val binWt: DBinWt = binDat._2
+			val weightedEDSq = binWt.*(estimDiffSq)
+			weightedEDSq
+		})
+		val summedWSDs = weightedSquaredDevs.reduce((ltSum, rtSum) => ltSum.+(rtSum))
+		summedWSDs
+	}
+	def calcReturnAtVec(assetWeights : Seq[HoldWt], assetReturns : Seq[AssetRet]) : AnnRet = {
+		assert(assetWeights.length == assetReturns.length)
+		val paired = assetWeights.zip(assetReturns)
+		val totRet : AnnRet = paired.foldLeft(BigDecimal("0.0"))((sum, pair) => {
+			val (holdWt, assetRet) = pair
+			assert(holdWt._1 == assetRet._1)
+			sum.+(holdWt._2.*(assetRet._2))
+		})
+		totRet
+	}
+}
