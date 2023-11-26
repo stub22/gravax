438691695c861d7dabc865315dbb7363a8101e3a
stub22 <1082227+stub22@users.noreply.github.com>
2023-11-26 15:40:05 -0800

    Defining API prototype using Scala3, Tapir, http4s, sbt build

---
+++ grvx_sbt_sc3/grvx_dbin_api/build.sbt
@@ -0,0 +1,25 @@
+val tapirVersion = "1.8.1"
+
+lazy val rootProject = (project in file(".")).settings(
+  Seq(
+    name := "tapgen",
+    version := "0.1.0-SNAPSHOT",
+    organization := "com.appstract",
+    scalaVersion := "3.3.1",
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % tapirVersion,
+      "org.http4s" %% "http4s-ember-server" % "0.23.24",
+      "com.softwaremill.sttp.tapir" %% "tapir-prometheus-metrics" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
+
+      "com.softwaremill.sttp.tapir" %% "tapir-aws-lambda" % tapirVersion, // Docs say "1.9.0"
+      "com.softwaremill.sttp.tapir" %% "tapir-aws-sam" % tapirVersion, // "1.9.0"
+
+      "ch.qos.logback" % "logback-classic" % "1.4.11",
+      "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % tapirVersion % Test,
+      "org.scalatest" %% "scalatest" % "3.2.17" % Test,
+      "com.softwaremill.sttp.client3" %% "circe" % "3.9.1" % Test
+    )
+  )
+)
---
+++ grvx_sbt_sc3/grvx_dbin_api/src/main/scala/com/appstract/Main.scala
@@ -0,0 +1,38 @@
+package com.appstract
+
+import cats.effect.{ExitCode, IO, IOApp}
+import com.comcast.ip4s.{Host, Port, port}
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Router
+import sttp.tapir.server.http4s.{Http4sServerInterpreter, Http4sServerOptions}
+
+object Main extends IOApp:
+
+  override def run(args: List[String]): IO[ExitCode] =
+
+    val serverOptions: Http4sServerOptions[IO] =
+      Http4sServerOptions
+        .customiseInterceptors[IO]
+        .metricsInterceptor(Endpoints.prometheusMetrics.metricsInterceptor())
+        .options
+    val routes = Http4sServerInterpreter[IO](serverOptions).toRoutes(Endpoints.all)
+
+    val port = sys.env
+      .get("HTTP_PORT")
+      .flatMap(_.toIntOption)
+      .flatMap(Port.fromInt)
+      .getOrElse(port"8080")
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(Host.fromString("localhost").get)
+      .withPort(port)
+      .withHttpApp(Router("/" -> routes).orNotFound)
+      .build
+      .use { server =>
+        for {
+          _ <- IO.println(s"Go to http://localhost:${server.address.getPort}/docs to open SwaggerUI. Press ENTER key to exit.")
+          _ <- IO.readLine
+        } yield ()
+      }
+      .as(ExitCode.Success)
