1038bc9c609fd256be79585f14f4a703e2e8227c
stub22 <1082227+stub22@users.noreply.github.com>
2023-11-28 16:23:03 -0800

    Roughing out PairwiseProductMatrix

--- grvx_sbt_sc3/grvx_dbin_api/src/main/scala/fun/gravax/dbin/model/CovarModel.scala
+++ grvx_sbt_sc3/grvx_dbin_api/src/main/scala/fun/gravax/dbin/model/CovarModel.scala
@@ -1,8 +1,8 @@
 package fun.gravax.dbin.model
 
-import fun.gravax.dbin.model.CovarTypes.EntryCovariance
+import fun.gravax.dbin.model.CovarTypes.{EEProduct, EntryCovariance}
 import fun.gravax.dbin.model.DbinModelTypes.{BinRelWt, EntryKey, EntryMean, EntryVariance}
-import fun.gravax.dbin.model.PortfolioTypes.{AssetWeight, ExpPortfSquare, PortfMean, PortfVar, PortfSquaredMean}
+import fun.gravax.dbin.model.PortfolioTypes.{AssetWeight, ExpPortfSquare, PortfMean, PortfSquaredMean, PortfVar}
 
 class CovarModel {
 
@@ -10,6 +10,7 @@ class CovarModel {
 
 object CovarTypes {
 	type EntryCovariance = BigDecimal
+	type EEProduct = BigDecimal
 }
 
 // If we assume that CovarMatrix "has access" to the required data, then we are thinking in OOP style.
@@ -19,10 +20,44 @@ trait CovarMatrix {
 	def getKeys : Seq[EntryKey]
 }
 
+trait PairwiseProductMatrix {
+	def getExpectedProductOrThrow(e1: EntryKey, e2: EntryKey): EEProduct
+
+	def getKeys: Seq[EntryKey]
+}
+
 trait BinCovarOps {
-	def estimCovars(globalMeans : Map[EntryKey, EntryMean], weightedBins: Seq[(BinRelWt, Bin)]) : CovarMatrix
+
+	def estimCovarMatrix(entryKeys: Seq[EntryKey])(parentBin: Bin, numLevels: Int): CovarMatrix = {
+		// A) Get the pairwiseProductMatrix from the appropriate child level.
+		// B) Get the global means from parentBin, and make a pairwise matrix of products from that.
+		// Subtract matrix B from A.
+		???
+	}
 
 
+	def estimPairwiseProducts(entryKeys : Seq[EntryKey])(parentBin : Bin, numLevels : Int) : PairwiseProductMatrix = {
+		if (numLevels == 0) {
+			// Just multiply out the expected values in parentBin
+			???
+		} else {
+			// Access some bin source to get the bottom-level bins, paired with their weights relative to parentBin.
+			// Get all their pairwise products, and make their weighted sum.
+			// This can work as a recursive fetch, or we can simply jump to the bottom-level, as long as we have
+			// access to the necessary weighting information.
+			???
+		}
+	}
+
+	def expSquareForOneEntryInOneBin(entryKey : EntryKey, bin: Bin) = ???
+
+	def expProductInBin(bin : Bin, subLevels : Int)(ekA : EntryKey, ekB : EntryKey) : EEProduct = {
+		// If subLevels is 0, then (for A != B) exp(A*B) == exp(A) * exp(B).  Right?
+		// But for A==B, we have exp(A*A) = exp(A)^2 + var(A)
+		// As alternative to subLevels, we might pass in the local covariance estimate.
+		// Should Bin carry an optional covariance estimate?
+		???
+	}
 }
 
 object PortfolioTypes {
@@ -35,26 +70,47 @@ object PortfolioTypes {
 trait PortfolioOps {
 
 	def estimPortfolioVariance(pweights: Map[EntryKey, AssetWeight], globalMeans: Map[EntryKey, EntryMean],
-							   weightedBins: Seq[(BinRelWt, Bin)]): EntryVariance
+							   weightedBins: Seq[(BinRelWt, Bin)]): EntryVariance = ???
 
 	// pweights and binWeights may be assumed to be Convex (i.e. sum to 1.0).
 	// Let's note exactly when this assump needs to be applied.
-	def estimPortfolioVariance(pweights: Map[EntryKey, AssetWeight], globalMeans: Map[EntryKey, EntryMean],
-							   covarMatrix : CovarMatrix): PortfVar = {
-		val pMean : PortfMean = ???
-		val pMeanSq : PortfSquaredMean = pMean.pow(2)
-		val expPSq : ExpPortfSquare = {
-			val ekeys = pweights.keys
-			???
-		}
-		val pVar = expPSq.-(pMeanSq)
-		pVar
+
+	// The key assumption here is that the portfolio numbers must be summable.
+	// So additive returns like 0.05, 0.00, -0.05 make sense, but not multiplicative ones like 0.95, 1.05.
+	def estimPortfolioVariance(pweights: Map[EntryKey, AssetWeight], covarMatrix : CovarMatrix): PortfVar = {
+		// https://en.wikipedia.org/wiki/Covariance#Covariance_of_linear_combinations
+		val ekvs: Seq[(EntryKey, AssetWeight)] = pweights.toSeq
+		val ekvtmps: Seq[((EntryKey, AssetWeight), (EntryKey, AssetWeight))] = triangularMatrixPairs(ekvs)
+		val terms: Seq[BigDecimal] = ekvtmps.map((pairA, pairB) => {
+			val (ekA, awA) = pairA
+			val (ekB, awB) = pairB
+			val covAB: EntryCovariance = covarMatrix.getCovarOrThrow(ekA, ekB)
+			// Terms on the diagonal are self-variance
+			// Terms off the diagonal are co-variance and need to be counted twice
+			val dblr : BigDecimal = if (ekA == ekB) BigDecimal(1) else BigDecimal(2)
+			val termAB = dblr.*(awA).*(awB).*(covAB)
+			termAB
+		})
+		val sum: PortfVar = terms.sum // Relies on summability of the separate returns
+		sum
+		// , globalMeans: Map[EntryKey, EntryMean],
+		// val pMean : PortfMean = ???
+		//val pMeanSq : PortfSquaredMean = pMean.pow(2)
+		// val expPSq : ExpPortfSquare = {
+			// val ekeys: Seq[EntryKey] = pweights.keys.toSeq
+			// val ekeyTMPs: Seq[(EntryKey, EntryKey)] = triangularMatrixPairs(ekeys)
+			// ???
+	//	}
+		// val pVar = expPSq.-(pMeanSq)
+		// pVar
 	}
 
 	def estimPortfolioMean(pweights: Map[EntryKey, AssetWeight], globalMeans: Map[EntryKey, EntryMean]) : PortfMean = {
 		// Easy - just take the dot-product:  Multiply each mean by appropriate weight, and sum.
 		???
 	}
+
+	def triangularMatrixPairs[A](inSeq : Seq[A]) : Seq[(A,A)] = ???
 
 
 }
--- grvx_sbt_sc3/grvx_dbin_api/src/test/scala/fun/gravax/dbin/tapend/MathSuite.scala
+++ grvx_sbt_sc3/grvx_dbin_api/src/test/scala/fun/gravax/dbin/tapend/MathSuite.scala
@@ -1,8 +1,10 @@
 package fun.gravax.dbin.tapend
 
-import cats.instances.list._
-import cats.syntax.all._
-
+import cats.instances.list.*
+import cats.syntax.all.*
+import fun.gravax.dbin.model.DbinModelTypes.{BinRelWt, EntryKey, EntryMean, EntryVariance}
+import fun.gravax.dbin.model.PortfolioTypes.AssetWeight
+import fun.gravax.dbin.model.{Bin, BinCovarOps, BinEntry, PortfolioOps}
 import munit.FunSuite
 
 class MathSuite extends FunSuite {
@@ -30,4 +32,17 @@ class MathSuite extends FunSuite {
 		// xxs=[(1,1), (1,2), (1,3), (1,4), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (3,3), (3,4), (4,1), (4,2), (4,3), (4,4)]
 	}
 
+	val (symG, symA, symM, symS) = ("GOOG", "AAPL", "MSFT", "SPY")
+
+	test("pvar") {
+		val pwts = Map(symG -> bd("0.2"), symA -> bd("0.15"), symM -> bd("0.22"), symS -> bd("0.43"))
+		val pops = new PortfolioOps {}
+		val bcops = new BinCovarOps {}
+		val means01 = Map(symG -> bd("0.1"), symA -> bd("0.2"), symM -> bd("0.3"), symS -> bd("0.4"))
+
+		// val entsG = mkEntry(symG, "1)
+	}
+	def bd(numTxt : String) = BigDecimal(numTxt)
+	def mkEntry(sym : EntryKey, mean : EntryMean, vrnc : EntryVariance) : BinEntry = BinEntry(sym, mean, vrnc)
+	def mkEntry(sym : EntryKey, meanTxt : String, vrncTxt : String): BinEntry = mkEntry(sym, bd(meanTxt), bd(vrncTxt))
 }
